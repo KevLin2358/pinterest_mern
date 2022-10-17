3d991d660d50c3204994dcb72292146f6826c527
KevLin2358 <klin2358@gmail.com>
2022-10-17 16:16:18 -0400

    frontend setup

---
+++ frontend/src/components/main/main_page.js
@@ -0,0 +1,19 @@
+// src/components/main/main_page.js
+
+import React from 'react';
+
+class MainPage extends React.Component {
+
+  render() {
+    return (
+      <div>
+        <h1>A Pinterest Clone</h1>
+        <footer>
+          Copyright &copy; 2021 Pin
+        </footer>
+      </div>
+    );
+  }
+}
+
+export default MainPage;
