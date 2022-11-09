a2fe23cac7095cd87ac22046224e0a9ca417f9e2
Sunny Mei <sunmeiappprep@gmail.com>
2022-11-09 08:46:32 -0500

    need to work updates styling

--- frontend/src/components/modal/modal.js
+++ frontend/src/components/modal/modal.js
@@ -1,10 +1,18 @@
 
 import React from "react";
 import "./modal.css";
+import { useState, useEffect, useRef } from 'react'
 import jp from "../../imageComponent/images/jpart.jpg"
 import { Link } from "react-router-dom";
 import FusedImages from "../fusedImages/fusedImages";
 function Modal({ setOpenModal }) {
+    const [height, setHeight] = useState(0)
+    const ref = useRef(null)
+
+    useEffect(() => {
+        setHeight(ref.current.clientHeight+30)
+      })
+
   return (
     <div className="modalBackground">
       <div className="modalContainer">
@@ -17,11 +25,12 @@ function Modal({ setOpenModal }) {
             X
           </button>
         </div> */}
-        <div className="title">
-          <h1>Updates</h1>
+        <div ref={ref} className="title">
+          <h1 >Updates</h1>
+          {height}
         </div>
         <div className="body">
-            <div><Link to="/today"><FusedImages/></Link></div>
+            <div className="testing" style={{marginTop: "30vh"}}><Link to="/today"><FusedImages/></Link></div>
             <div><Link to="/today"><FusedImages/></Link></div>
             <div><Link to="/today"><FusedImages/></Link></div>
             <div><Link to="/today"><FusedImages/></Link></div>
