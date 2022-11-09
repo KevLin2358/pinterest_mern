8394b906003f5024f97ba6949b4084e56e0ae159
Sunny Mei <sunmeiappprep@gmail.com>
2022-11-09 03:59:59 -0500

    redirect works , not sure why overflow messthings up

--- frontend/src/components/modal/modal.js
+++ frontend/src/components/modal/modal.js
@@ -2,11 +2,12 @@
 import React from "react";
 import "./modal.css";
 import jp from "../../imageComponent/images/jpart.jpg"
+import { Link } from "react-router-dom";
 function Modal({ setOpenModal }) {
   return (
     <div className="modalBackground">
       <div className="modalContainer">
-        <div className="titleCloseBtn">
+        {/* <div className="titleCloseBtn">
           <button
             onClick={() => {
               setOpenModal(false);
@@ -14,21 +15,30 @@ function Modal({ setOpenModal }) {
           >
             X
           </button>
-        </div>
+        </div> */}
         <div className="title">
           {/* <h1>Are You Sure You Want to Continue?</h1> */}
         </div>
         <div className="body">
+            <div className="bellArt"><Link to="/today"><img src={jp} alt="Logo" /></Link></div>
+            <div className="bellArt"><Link to="/"><img src={jp} alt="Logo" /></Link></div>
+            <div className="bellArt"><Link to="/profile"><img src={jp} alt="Logo" /></Link></div>
+            <div className="bellArt"><Link to="/today"><img src={jp} alt="Logo" /></Link></div>
+            <div className="bellArt"><Link to="/"><img src={jp} alt="Logo" /></Link></div>
+            {/* <div className="bellArt"><Link to="/profile"><img src={jp} alt="Logo" /></Link></div> */}
+            {/* <div className="bellArt"><Link to="/today"><img src={jp} alt="Logo" /></Link></div> */}
+            {/* <div className="bellArt"><Link to="/"><img src={jp} alt="Logo" /></Link></div> */}
+            {/* <div className="bellArt"><Link to="/profile"><img src={jp} alt="Logo" /></Link></div> */}
+            {/* <div className="bellArt"><Link to="/today"><img src={jp} alt="Logo" /></Link></div> */}
+            {/* <div className="bellArt"><img src={jp} alt="Logo" /></div>
             <div className="bellArt"><img src={jp} alt="Logo" /></div>
             <div className="bellArt"><img src={jp} alt="Logo" /></div>
             <div className="bellArt"><img src={jp} alt="Logo" /></div>
             <div className="bellArt"><img src={jp} alt="Logo" /></div>
-            <div className="bellArt"><img src={jp} alt="Logo" /></div>
-            <div className="bellArt"><img src={jp} alt="Logo" /></div>
-            <div className="bellArt"><img src={jp} alt="Logo" /></div>
-          <p>The next page looks amazing. Hope you want to go there!</p>
+            <div className="bellArt"><img src={jp} alt="Logo" /></div> */}
+          {/* <p>The next page looks amazing. Hope you want to go there!</p> */}
         </div>
-        <div className="footer">
+        {/* <div className="footer">
           <button
             onClick={() => {
               setOpenModal(false);
@@ -38,7 +48,7 @@ function Modal({ setOpenModal }) {
             Cancel
           </button>
           <button>Continue</button>
-        </div>
+        </div> */}
       </div>
     </div>
   );
