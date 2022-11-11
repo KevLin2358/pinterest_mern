a9232dff3a4068fd32f6a30180bacbc9db52bf75
Sunny Mei <sunmeiappprep@gmail.com>
2022-11-11 01:30:28 -0500

    spawning changes need work

--- frontend/src/pages/homepage.js
+++ frontend/src/pages/homepage.js
@@ -2,14 +2,87 @@ import React from 'react';
 import Navbar from '../components/nav/navbar';
 import Pins from '../components/pin/pins';
 import "./homepage.css"
+import { useState, useEffect, useRef } from 'react'
+
 const Home = () => {
+// const number = 1000;
+const [height, setHeight] = useState(0)
+const ref = useRef(null)
+const heightInString = height+"px"
+
+    useEffect(() => {
+        setHeight(ref.current.clientHeight+ref.current.clientHeight*.5)
+        console.log(height)
+    })
 
     return (
         <React.Fragment>
             <Navbar/>
-            <div className='homePageContainer'>
+            <div ref={ref} className='homePageContainer'>
                 <div className='homePageBodyFlex'>
-                    <div className='homePageBody'>
+                    <div style={{maxHeight: heightInString}} className='homePageBody'>
+                        <Pins/>
+                        <Pins/>
+                        <Pins/>
+                        <Pins/>
+                        <Pins/>
+                        <Pins/>
+                        <Pins/>
+                        <Pins/>
+                        <Pins/>
+                        <Pins/>
+                        <Pins/>
+                        <Pins/>
+                        <Pins/>
+                        <Pins/>
+                        <Pins/>
+                        <Pins/>
+                        <Pins/>
+                        <Pins/>
+                        <Pins/>
+                        <Pins/>                        <Pins/>
+                        <Pins/>
+                        <Pins/>
+                        <Pins/>
+                        <Pins/>                        <Pins/>
+                        <Pins/>
+                        <Pins/>
+                        <Pins/>
+                        <Pins/>                        <Pins/>
+                        <Pins/>
+                        <Pins/>
+                        <Pins/>
+                        <Pins/>                        <Pins/>
+                        <Pins/>
+                        <Pins/>
+                        <Pins/>
+                        <Pins/>                        <Pins/>
+                        <Pins/>
+                        <Pins/>
+                        <Pins/>
+                        <Pins/>                        <Pins/>
+                        <Pins/>
+                        <Pins/>
+                        <Pins/>
+                        <Pins/>                        <Pins/>
+                        <Pins/>
+                        <Pins/>
+                        <Pins/>
+                        <Pins/>                        <Pins/>
+                        <Pins/>
+                        <Pins/>
+                        <Pins/>
+                        <Pins/>                        <Pins/>
+                        <Pins/>
+                        <Pins/>
+                        <Pins/>
+                        <Pins/>                        <Pins/>
+                        <Pins/>
+                        <Pins/>
+                        <Pins/>
+                        <Pins/>
+                    </div>
+                    {/* <div className='homePageBody'>
                         <Pins/>
                         <Pins/>
                         <Pins/>
@@ -86,13 +159,7 @@ const Home = () => {
                         <Pins/>
                         <Pins/>
                         <Pins/>
-                    </div>
-                    <div className='homePageBody'>
-                        <Pins/>
-                        <Pins/>
-                        <Pins/>
-                        <Pins/>
-                    </div>
+                    </div> */}
                 </div>
 
             </div>
