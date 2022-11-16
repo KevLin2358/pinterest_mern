c87a0d974e8aa9e96d841d4b43e8f92060e68e59
KevLin2358 <klin2358@gmail.com>
2022-11-16 11:03:31 -0500

    added pin reducer

---
+++ frontend/src/reducers/pin_reducer.js
@@ -0,0 +1,36 @@
+import {
+  RECEIVE_PINS,
+  RECEIVE_USER_PINS,
+  RECEIVE_PIN,
+  REMOVE_PIN
+} from '../actions/pin_actions';
+
+const pinReducer = (state = {}, action) =>{
+  Object.freeze(state);
+  let newState = Object.assign({}, state);
+
+  let pins = undefined;
+  if(action.pins){
+    pins = action.pins.data
+  }
+
+  let nextState = {};
+  switch(action.type){
+    case RECEIVE_PINS:
+        return {...action.pins};
+    case RECEIVE_USER_PINS:
+      pins.forEach((pin) => {
+        nextState[pin.id]=pin //question needed 
+      })
+      return nextState;
+    case RECEIVE_PIN:
+      return action.pin;
+    case REMOVE_PIN:
+      delete newState[action.id]
+      return newState;
+    default:
+      return state;
+  }
+}
+
+export default pinReducer;
