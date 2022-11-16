c833d635b07cf825f688d3061fe3dd9cfb22afb0
Sunny Mei <sunmeiappprep@gmail.com>
2022-11-16 11:05:32 -0500

    create form

---
+++ frontend/src/components/createForm/createForm.js
@@ -0,0 +1,54 @@
+import React from "react";
+import { useState } from "react";
+function CreateForm() {
+    const [userID,setUserID] = useState("")
+    const [image,setImage] = useState("")
+    const [text,setText] = useState("")
+    const [description,setdescription] = useState("")
+    const [link,setLink] = useState("")
+
+  return (
+    <div>
+        CreateForm
+        <form>
+            <input
+            value={userID}
+            placeholder="userid"
+            onChange={(e) => setUserID(e.target.value)}
+            >           
+            </input>
+
+            <input
+            value={image}
+            placeholder="image"
+            onChange={(e) => setImage(e.target.value)}
+            >           
+            </input>
+
+            <input
+            value={text}
+            placeholder="text"
+            onChange={(e) => setText(e.target.value)}
+            >           
+            </input>
+
+            <input
+            value={description}
+            placeholder="description"
+            onChange={(e) => setdescription(e.target.value)}
+            >           
+            </input>
+
+            <input
+            value={link}
+            placeholder="link"
+            onChange={(e) => setLink(e.target.value)}
+            >           
+            </input>
+        </form>
+    </div>
+  )
+}
+
+export default CreateForm
+
